3b3d3b2032060936d3c7fefdcb1d174a57c3f78c
Santiago Aguilar <130328638+Santiag0Aguilar@users.noreply.github.com>
2025-03-23 12:10:14 -0600

    Add files via upload

---
+++ build/scripts/Theme.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  ThemeSelected();
+});
+
+function ThemeSelected() {
+  const BtnThemeSelected = document.querySelectorAll(
+    ".Theme__Option--Selected"
+  );
+  const body = document.querySelector("Body");
+
+  BtnThemeSelected.forEach((radio) => {
+    radio.addEventListener("change", () => {
+      console.log(typeof radio.value);
+      if (radio.value == "2") {
+        body.classList.remove("Theme3");
+        body.classList.add("Theme2");
+      } else if (radio.value == "3") {
+        body.classList.remove("Theme2");
+        body.classList.add("Theme3");
+      } else {
+        body.classList.remove("Theme2");
+        body.classList.remove("Theme3");
+      }
+    });
+  });
+  console.log(BtnThemeSelected);
+}
